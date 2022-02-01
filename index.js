7d4ff6d6487972c1e03d66b8190e5f66ced6e5b7
Roman <roman.onyshchuk@nure.ua>
2022-02-01 02:04:02 +0200

    Third commit *Zombie child class of 'Entity' was added; *Zombie moving schema was added; *Fixed a bug when zombies were shaking on the same axis with human; *Zombies are pursueing the human!; *Feature of spawning was improved; *Entities are now rotating when choose to move left or right;

--- index.js
+++ index.js
@@ -46,14 +46,15 @@ class Entity{
         this.name = name;
     }
 
-    moveCheck = setInterval(()=>{
+    moving(){
+        var moveCheck = setInterval(()=>{
         if (wPressed){
             if(this.object.offsetTop <= mainArea.offsetTop){
                 this.object.style.top = mainArea.offsetTop + 'px';
                 return;
             }
             else{
-                this.object.style.top=parseInt(this.object.offsetTop) - 10 +'px';
+                this.object.style.top=parseInt(this.object.offsetTop) - this.speed +'px';
             }
         }
     
@@ -67,16 +68,17 @@ class Entity{
                 return;
             }
             else{
-                this.object.style.left=parseInt(this.object.offsetLeft) - 10 +'px';
+                this.object.style.left=parseInt(this.object.offsetLeft) - this.speed +'px';
             }
         }
     
         if (sPressed){
-            if(parseInt(this.object.offsetTop + this.object.offsetHeight) + 'px' >= parseInt(mainArea.offsetHeight + mainArea.offsetTop) + 'px'){
+            if(parseInt(this.object.offsetTop + this.object.offsetHeight) + 'px' >= parseInt(mainArea.offsetHeight + mainArea.offsetTop - 15) + 'px'){
+                this.object.style.top = parseInt(mainArea.offsetHeight + mainArea.offsetTop - this.object.offsetHeight - 15) + 'px';
                 return;
             }
             else{
-                this.object.style.top=parseInt(this.object.offsetTop) + 10 + 'px';
+                this.object.style.top=parseInt(this.object.offsetTop) + this.speed + 'px';
             }
         }
     
@@ -89,14 +91,15 @@ class Entity{
                 this.object.style.left = parseInt(mainArea.offsetLeft + mainArea.offsetWidth - this.object.offsetWidth) + 'px';
             }
             else{
-                this.object.style.left = parseInt(this.object.offsetLeft) + 10 + 'px';
+                this.object.style.left = parseInt(this.object.offsetLeft) + this.speed + 'px';
             }
         }
     }, 50)
+    }
 }
 
 class Human extends Entity{
-    type = "soldier";
+    type = "human";
     object = document.createElement("div");
 
     constructor(name, health, speed, damage, skin = "url(img/soldier2.png)"){
@@ -115,9 +118,107 @@ class Human extends Entity{
 
         this.object.style.top = parseInt(this.object.offsetHeight*3 + mainArea.offsetTop) + 'px';
         this.object.style.left = parseInt(this.object.offsetWidth*12 + mainArea.offsetLeft) + 'px';
+        this.moving();
     }
 
 }
 
-let John = new Human("John", 100, 10, 20);
+class Zombie extends Entity{
+    type = "zombie";
+    object = document.createElement("div");
+    target = "";
+
+
+    constructor(name, health, speed, damage, skin = "url(img/zombie2.png)"){
+        super(name);
+        this.health=health;
+        this.speed=speed;
+        this.damage=damage;
+        this.skin=skin;
+        this.object.style.background=skin;
+    }
+
+    spawn(){
+        this.object.className = this.type;
+        this.target=document.getElementById("human");
+        mainArea.appendChild(this.object);
+
+        var sides = new Array("left", "right");
+        var side = sides[Math.floor(Math.random() * 2)];
+        if(side == "left"){
+            this.object.style.transform="rotateY(360deg)";
+            this.object.style.left = parseInt(mainArea.offsetLeft) + 'px';
+            this.object.style.top = parseInt(Math.random() * (90-10) + 10) + '%';
+
+            if(this.object.offsetTop <= mainArea.offsetTop){
+                this.object.style.top = mainArea.offsetTop + 'px';
+            }
+            if(parseInt(this.object.offsetTop + this.object.offsetHeight) + 'px' >= parseInt(mainArea.offsetHeight + mainArea.offsetTop) + 'px'){
+                this.object.style.top = parseInt(mainArea.offsetHeight + mainArea.offsetTop - this.object.offsetHeight) + 'px';
+            }
+        }
+        else{
+            this.object.style.transform="rotateY(180deg)";
+            this.object.style.left = parseInt(mainArea.offsetLeft + mainArea.offsetWidth - this.object.offsetWidth) + 'px';
+            this.object.style.top = parseInt(Math.random() * (90-10) + 10) + '%';
+
+            if(this.object.offsetTop <= mainArea.offsetTop){
+                this.object.style.top = mainArea.offsetTop + 'px';
+            }
+            if(parseInt(this.object.offsetTop + this.object.offsetHeight) + 'px' >= parseInt(mainArea.offsetHeight + mainArea.offsetTop) + 'px'){
+                this.object.style.top = parseInt(mainArea.offsetHeight + mainArea.offsetTop - this.object.offsetHeight) + 'px';
+            }
+
+
+        }
+        this.moving();
+    }
+
+    moving(){
+        var moveCheck = setInterval(()=>{
+            
+            if (this.target.offsetLeft > this.object.offsetLeft){
+                if(this.target.offsetLeft - this.object.offsetLeft <=30){
+                    
+                }
+                else{
+                    this.object.style.left = parseInt(this.object.offsetLeft + this.speed) + "px";
+                }
+
+                if(this.object.isTurned){
+                    this.object.style.transform="rotateY(360deg)";
+                    this.object.isTurned = false;
+                }
+            }
+            else{
+                if(this.object.offsetLeft - this.target.offsetLeft <=30){
+                    
+                }
+                else{
+                    this.object.style.left = parseInt(this.object.offsetLeft - this.speed) + "px";
+                }
+                if(!this.object.isTurned){
+                    this.object.style.transform="rotateY(180deg)";
+                    this.object.isTurned = true;
+                }
+            }
+            if(this.target.offsetTop > this.object.offsetTop){
+                if(this.target.offsetTop + this.target.offsetHeight - this.object.offsetTop <=10){
+                    return;
+                }
+                this.object.style.top = parseInt(this.object.offsetTop + this.speed) + "px";
+            }
+            else{
+                if(this.object.offsetTop - this.target.offsetTop <=10){
+                    return;
+                }
+                this.object.style.top = parseInt(this.object.offsetTop - this.speed) + "px";
+            }
+        }, 50)
+    }
+}
+
+let Bill = new Human("Bill", 100, 10, 20)
+Bill.spawn();
+let John = new Zombie("John", 100, 5, 20);
 John.spawn();
